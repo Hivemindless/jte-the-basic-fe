7a51ea38a094363eab01470d160a11a8a544b593
Hivemindless <giogeta87@gmail.com>
2021-09-21 15:47:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var static = require('node-static');
 var http = require('http');
 
-var file = new(static.Server)(__dirname);
+var file = new(static.Server)(__dirname + "/dist/");
+
+const port = 8081;
+
+function format_date(){
+  var date = new Date();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var year = date.getFullYear();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+
+  return day + '/' + month + '/' + year + ' - ' + hour + ':' + minute + ':' + second;
+}
 
 http.createServer(function (req, res) {
+
+  console.log( format_date() +' request at port: ' + port)
   file.serve(req, res);
-}).listen(8080);
+
+}).listen(port);
+
+console.log( format_date() + " Server started")
